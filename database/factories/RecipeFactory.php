79aa9df047e233bccde554721a2fe695ddf3fa48
Joren lauryssen <joren.etw@gmail.com>
2021-03-21 09:12:43 +0100

    working, does create duplicates, skipping this for now

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\Recipe;
 use FakerRestaurant\Provider\en_US\Restaurant;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -26,7 +27,7 @@ class RecipeFactory extends Factory
 
         return [
             'name' => $this->faker->foodName(),
-            'category_id' => 1
+            'category_id' => Category::inRandomOrder()->first()->id
         ];
     }
 }
