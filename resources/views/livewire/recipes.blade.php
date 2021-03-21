dc2fad1da29cab547a90ab363b81d07996dc90ea
Joren lauryssen <joren.etw@gmail.com>
2021-03-21 11:35:42 +0100

    Homepage table - added bootstrap (using CDN, is easier for now)

--- resources/views/livewire/recipes.blade.php
+++ resources/views/livewire/recipes.blade.php
@@ -1,3 +1,22 @@
 <div>
-    {{-- Be like water. --}}
+    <table class="table table-bordered mt-5">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($recipes as $recipe)
+            <tr>
+                <td>{{ $recipe->id }}</td>
+                <td>{{ $recipe->name }}</td>
+                <td>
+                    <button class="btn btn-info btn-sm">View</button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
