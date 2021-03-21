92b4023dbcb2fb1e0ea6a8379baf0a6b96cfd6ad
Joren lauryssen <joren.etw@gmail.com>
2021-03-21 15:35:32 +0100

    Multiplier must always be an integer

--- app/Listeners/CalculateFraction.php
+++ app/Listeners/CalculateFraction.php
@@ -2,22 +2,26 @@
 
 namespace App\Listeners;
 
+use Illuminate\Support\Facades\Log;
 use Phospr\Fraction;
 
 class CalculateFraction
 {
-    static function handle($fraction, $multiplier)
+    static function handle(string $fraction, int $multiplier)
     {
+        Log::debug($fraction);
         // Check if value is indeed a fraction
         if (strpos($fraction, '/') === false) {
             return $fraction * $multiplier;
         }
 
-        $fraction = new Fraction((int)explode('/', $fraction)[0], (int)explode('/', $fraction)[1]);
+        $fraction = new Fraction(...array_map('intval', explode('/', $fraction)));
 
         if ($fraction->isInteger()) {
             return $fraction * $multiplier;
         }
+
+        Log::debug('Wil multiply with ' . $multiplier);
 
         return $fraction->multiply(new Fraction($multiplier, 1));
     }
